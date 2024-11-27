9cca53d3ce0e7978a2a75474dc511449cf1199ca
Joel Panjaitan <joel.martuaraja@praktis.co>
2024-11-27 10:19:20 +0700

    update for backend repository files

---
+++ config/database.go
@@ -0,0 +1,21 @@
+package config
+
+import (
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DataBase*gorm.DB
+
+func InitDB() *gorm.DB {
+	network := "admin:password@tcp(database-storage.crasa40cmvqz.ap-southeast-2.rds.amazonaws.com:3306)/kreasitech?charset=utf8mb4&parseTime=True&loc=Local"
+	DB, err := gorm.Open(mysql.Open(network), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Connection failed to database: %v", err)
+	}
+	
+	DataBase= DB
+	return DB
+}
