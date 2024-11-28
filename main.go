1ff88508684bb64ccaff53bf6278e042b94d9333
Joel Panjaitan <joel.martuaraja@praktis.co>
2024-11-28 12:37:35 +0700

    update for backend repository

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"kreasitech-backend/config"
+	"kreasitech-backend/routes"
+
+	"github.com/gorilla/handlers"
+)
+
+func main(){
+	database := config.InitDB()
+	defer func() {
+		sqlDB, err := database.DB()
+		if err != nil {
+			log.Fatalf("Error accessing underlying sql.DB: %v", err)
+		}
+		sqlDB.Close() 
+	}()
+
+	router := routes.RegisteredRoutes()
+
+	headersOk := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"})
+	originsOk := handlers.AllowedOrigins([]string{"http://localhost:3000", "https://magenta-smakager-3db320.netlify.app"})
+	methodsOk := handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE", "OPTIONS"})
+
+	handleRouterCORS :=handlers.CORS(originsOk, headersOk, methodsOk)(router)
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		<-quit
+		log.Println("Shutting down server...")
+		os.Exit(0)
+	}()
+
+	log.Println("Development server is on port: 8080...")
+	if err := http.ListenAndServe(":8080",handleRouterCORS); err!= nil {
+		log.Fatalf("Development server failed: %v",err)
+	}
+}
